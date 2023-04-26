c0d1ad985259a4031f5ad1dc5acf21f45d5bd179
scriptalert(0)/script <72747112+PowfuArras@users.noreply.github.com>
2023-04-26 08:01:29 -0500

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         ChatGPT Themes
+// @namespace    jekyll.arw.gpt
+// @version      1.0
+// @description  A userscript that adds custom themes and a few other additional customization options to ChatGPT.
+// @author       Jekyll
+// @match        *://chat.openai.com/*
+// @downloadURL  https://raw.githubusercontent.com/PowfuArras/ChatGPT-Themes/master/userscript.js
+// @updateURL    https://raw.githubusercontent.com/PowfuArras/ChatGPT-Themes/master/userscript.js
+// @run-at       document-start
+// ==/UserScript==
+
+console.log("Hello World");
